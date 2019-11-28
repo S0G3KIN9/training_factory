bef2d35fa02d41ef584c9beb595df1b1c3438ea9
S0G3KIN9 <302185561@student.rocmondriaan.nl>
2019-11-28 13:01:10 +0100

    laatste

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,9 +3,11 @@
 namespace App\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AbstractAdapter;
 use Symfony\Component\HttpFoundation\Response;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     
     /**
@@ -20,9 +22,8 @@ class ArticleController
      * @Route("/news/{slug}")
      */
     public function show($slug){
-        return new Response(sprintf(
-            'Future page to show the article: "%s"',
-            $slug
-        ));
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            ]);
     }
 }
