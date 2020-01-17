a090b25ac3f1f61227a6719f3980f89ef2020a0e
S0G3KIN9 <302185561@student.rocmondriaan.nl>
2020-01-17 12:08:48 +0100

    homeee

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\UserRegistrationFormType;
 use App\Security\LoginFormAuthenticator;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,6 +18,8 @@ class SecurityController extends AbstractController
 {
     /**
      * @Route("bezoeker/login", name="app_login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -34,50 +37,16 @@ class SecurityController extends AbstractController
 
     /**
      * @Route("/logout", name="app_logout")
+     * @throws Exception
      */
     public function logout()
     {
-        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
+        throw new Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
 
     }
-
-    /**
-     * @Route("/bezoeker/register", name="registerpage")
-     * @param Request $request
-     * @param UserPasswordEncoderInterface $passwordEncoder
-     * @param GuardAuthenticatorHandler $guardHandler
-     * @param LoginFormAuthenticator $formAuthenticator
-     * @return Response
-     */
-
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $formAuthenticator)
-    {
-        $form = $this->createForm(UserRegistrationFormType::class);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            /** @var User $user */
-            $user = $form->getData();
-            $user->setPassword($passwordEncoder->encodePassword(
-                $user,
-                $user->getPassword()
-            ));
-
-            return $this->render('bezoeker/register.html.twig', [
-                'registrationForm' => $form->createView(),
-            ]);
-
-
-
-        }
-        return $this->render('bezoeker/register.html.twig', [
-            'registrationForm' => $form->createView(),
-        ]);
-
-    }
-
 
 }
+
 
 
 
